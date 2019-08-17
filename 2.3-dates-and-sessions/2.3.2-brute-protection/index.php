94560f868c997d2e71436a0d63ea75dd3c4b0a73
YULIA BORDACHEVA <s_proza@list.ru>
2019-08-17 22:59:00 +0300

    task 2.3.2 corrected

--- 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
+++ 2.3-dates-and-sessions/2.3.2-brute-protection/index.php
@@ -4,32 +4,75 @@ $users = [
     'randomUser' => 'somePassword',
     'janitor' => 'nimbus2000'
 ];
+function login($users) {
 
+    $login = isset($_POST['login']) ? $_POST['login'] : '';
 
-
-$login = $_POST["login"];
-$pass = $_POST["password"];
-$ip = '192.168.1.0';
-foreach ($users as $key => $value) {
-    if ($key === $login && $pass === $value) {
-        echo "Авторизация прошла успешно!";
-        return true;
+    if (array_key_exists($login, $users)) {
+        if ($users[$_POST['login']] == $_POST['password']) {
+            echo 'Вы успешно вошли';
+            return true;
+        }
+    }
+    echo 'Неверный логин или пароль';
+    return false;
+}
+function check($users) {
+    if (login($users) === true) {
+        exit;
+    } else {
+        $_SESSION['login'] = (isset($_POST['login']));
+        $_SESSION['time'] = time();
+        $_SESSION['counter'] = 1;
+        return;
     }
 }
-$currentTime = time();
-$timeFromCookies = $_COOKIE["date"];
-if ($currentTime - $timeFromCookies < 5) {
-    $date = date('d.m.Y H:i:s');
-    $file = fopen("${login}", 'a');
-    fwrite($file, "${date} : ${$ip} \n");
-    fclose($file);
-    echo "Слишком часто вводите пароль. Попробуйте еще раз через минуту";
-    return false;
-} else {
-    setcookie("date", time());
-    setcookie("ip", $ip);
-    $_SESSION["date"] = time();
-    $_SESSION["ip"] = $ip;
-    echo "Неверный логин или пароль!";
-    return false;
+function logFile() {
+    $file = 'data.txt';
+    $userFile = fopen($file, 'a');
+    $date = " " . "Попытка входа" . (isset($_POST['login'])) . ': ' . date('d.m.Y H:i:s') . "\n";
+    fwrite($userFile, $date);
+    fclose($userFile);
 }
+function countOfTry($users) {
+    session_set_cookie_params(1800);
+    session_start();
+
+    if (count($_SESSION) == 0) {
+        check($users);
+        return NULL;
+    }
+
+    if ($_SESSION['login'] === (isset($_POST['login']))) {
+
+        $_SESSION['counter']++;
+
+        if (((time() - $_SESSION['time']) <= 5) && ($_SESSION['counter'] === 2)) {
+
+            echo 'Слишком много попыток, попробуйте еще раз через минуту';
+            logFile();
+
+        } elseif ((time() - $_SESSION['time']) < 60) {
+            $_SESSION['counter']++;
+            if ($_SESSION['counter'] > 3) {
+                $_SESSION['counter'] = 0;
+                echo 'Слишком много попыток, попробуйте еще раз через минуту';
+                logFile();
+            } else {
+                echo 'Неверный логин или пароль';
+                return false;
+            }
+
+        } elseif ((time() - $_SESSION['time']) > 60) {
+            $_SESSION = [];
+            check($users);
+            return NULL;
+        }
+
+    } else {
+        check($users);
+        return NULL;
+    }
+}
+
+countOfTry($users);
